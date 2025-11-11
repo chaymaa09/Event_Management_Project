e6710ac25768c3b13e64168a2b17b4baf13e8329
chaymaa09 <chaymaa.akrraye-etu@etu.univh2c.ma>
2025-11-11 22:49:23 +0100

    before pulling angular

---
+++ EventMangementProject/src/main/java/com/example/eventmanagementproject/web/UserContoller.java
@@ -0,0 +1,42 @@
+package com.example.eventmanagementproject.web;
+
+import com.example.eventmanagementproject.dao.entities.User;
+import com.example.eventmanagementproject.service.TagService;
+import com.example.eventmanagementproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserContoller {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
