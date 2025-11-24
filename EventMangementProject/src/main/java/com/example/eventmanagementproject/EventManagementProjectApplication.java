703676133a669c7eeb413e5e9de8d77597f8b391
chaymaa09 <chaymaa.akrraye-etu@etu.univh2c.ma>
2025-11-24 12:38:28 +0100

    fixing application file

--- EventMangementProject/src/main/java/com/example/eventmanagementproject/EventManagementProjectApplication.java
+++ EventMangementProject/src/main/java/com/example/eventmanagementproject/EventManagementProjectApplication.java
@@ -1,5 +1,7 @@
 package com.example.eventmanagementproject;
 
+import com.example.eventmanagementproject.dao.entities.Event;
+import com.example.eventmanagementproject.dao.repositories.EventRepository;
 import com.example.eventmanagementproject.dao.entities.User;
 import com.example.eventmanagementproject.dao.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
